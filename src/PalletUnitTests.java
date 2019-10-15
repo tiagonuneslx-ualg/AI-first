6f7f2f1aed8fc7fb3a6ca10d7971c1ecab10c088
Rafael Duarte <rafaelalexandreisidoro@gmail.com>
2019-10-15 17:28:36 +0100

    Added Dim3 tests to PalletUnitTests

--- src/PalletUnitTests.java
+++ src/PalletUnitTests.java
@@ -133,4 +133,61 @@ public class PalletUnitTests {
         assertEquals(1, result);
     }
 
+    @Test
+    public void testSolveIDA_dim3_1() {
+        Pallet initial = new Pallet("123456789", 3);
+        Pallet goal = new Pallet("213456789", 3);
+        BestFirst s = new BestFirst();
+        Iterator<BestFirst.State> it = s.solveIDA(initial, goal);
+        int result = -1;
+        if (it == null)
+            System.out.println("no solution was found");
+        else {
+            while (it.hasNext()) {
+                BestFirst.State i = it.next();
+                if (!it.hasNext())
+                    result = i.getG();
+            }
+        }
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testSolveIDA_dim3_2() {
+        Pallet initial = new Pallet("123456789", 3);
+        Pallet goal = new Pallet("143256789", 3);
+        BestFirst s = new BestFirst();
+        Iterator<BestFirst.State> it = s.solveIDA(initial, goal);
+        int result = -1;
+        if (it == null)
+            System.out.println("no solution was found");
+        else {
+            while (it.hasNext()) {
+                BestFirst.State i = it.next();
+                if (!it.hasNext())
+                    result = i.getG();
+            }
+        }
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testSolveIDA_dim3_3() {
+        Pallet initial = new Pallet("123456789", 3);
+        Pallet goal = new Pallet("576891324", 3);
+        BestFirst s = new BestFirst();
+        Iterator<BestFirst.State> it = s.solveIDA(initial, goal);
+        int result = -1;
+        if (it == null)
+            System.out.println("no solution was found");
+        else {
+            while (it.hasNext()) {
+                BestFirst.State i = it.next();
+                if (!it.hasNext())
+                    result = i.getG();
+            }
+        }
+        assertEquals(24, result);
+    }
+
 }
